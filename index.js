81e9091ce3291bfb75390c0a3c5a57dc0e34e188
Neelachari N H <neelachari34@gmail.com>
2023-12-15 09:32:59 +0530

    server-backend

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
+server.use(cors())
+server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
-server.listen(1234, () => {
-  console.log('JSON Server is running')
+
+const PORT = 8080
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
 })
